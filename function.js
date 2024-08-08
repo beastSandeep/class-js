fb4bc552dcc6ff7e054d40d3c1c77c74765cbd06
Sandeep Singh <sandeepsinghpreet80055@gmail.com>
2024-08-08 08:03:13 +0530

    Function basic

---
+++ function.js
@@ -0,0 +1,59 @@
+// function declaration
+function shopping() {
+  console.log("shopping started");
+  console.log("shopping done");
+}
+
+// running / invoking a function
+shopping();
+
+// when a function retruns something, we can store that thing while running a function and putting inside a variable.
+function twoPlusFour() {
+  let a = 2;
+  let b = 4;
+  return a + b;
+}
+
+let six = twoPlusFour();
+console.log(six);
+
+// we can accept data from outside
+// a, b are function parameters
+function add(a, b) {
+  return a + b;
+}
+function mult(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function c(a) {
+  console.log(a);
+}
+
+// we can consume func. inside func.
+// add(10, 20) is equals to 30
+// c(30)
+// first insided fucn. run then outer func. run
+// 10 and 20 are function argument
+c(add(10, 20));
+c(mult(10, 20));
+c(div(10, 20));
+c(sub(10, 20));
+
+function addArr(arr) {
+  let sum = 0;
+
+  for (const number of arr) {
+    sum = sum + number;
+  }
+
+  return sum;
+}
+
+const array = [1, 2, 3, 4, 5];
+c(addArr(array));
